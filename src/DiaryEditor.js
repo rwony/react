782b27af221ad7a41ddee793ab26d0c20e1204a2
rwony <dev.rwlee@gmail.com>
2022-05-05 22:42:22 +0900

    commit

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -5,7 +5,7 @@ const DiaryEditor = () => {
 
   return (
     <div className="DiaryEditor">
-      <h2>오늘의 일기</h2>
+      <h2>오늘의 일기!</h2>
       <div>
         <input value={author} />
       </div>
