79538eca52e9eb9aea24601051ccb135be461a7c
rwony <88661435+rwony@users.noreply.github.com>
2022-07-05 22:31:55 +0900

    Add files via upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,9 +1,22 @@
-import "./App.css";
 import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello, React!!!</div>;
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="World wide web!"></Subject>
+        <TOC></TOC>
+        <Content
+          title="HTML"
+          desc="HTML is Hyper Text Markup Language."
+        ></Content>
+      </div>
+    );
   }
 }
 
