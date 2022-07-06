030bc2b0b5587f0a850f966ba188ee72ffc6c678
rwony <88661435+rwony@users.noreply.github.com>
2022-07-06 22:14:05 +0900

    Add files via upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -6,15 +6,43 @@ import Subject from "./components/Subject";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    // 컴포넌트가 실행 될 때 render() 함수보다 먼저 실행 되면서,
+    // 그 컴포넌트를 초기화 싶은 코드는 이(constructor) 안에 작성한다.
+    super(props);
+
+    // state 값 초기화
+    this.state = {
+      mode: "welcome",
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is Hyper Text Markup Language" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
+
   render() {
+    let _title,
+      _desc = null;
+
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title="WEB" sub="World wide web!"></Subject>
-        <TOC></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is Hyper Text Markup Language."
-        ></Content>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
