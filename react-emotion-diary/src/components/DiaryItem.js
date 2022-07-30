f646269ad0361771f57508c9467378d2c05d3233
rwony <88661435+rwony@users.noreply.github.com>
2022-07-30 19:13:47 +0900

    Update DiaryItem.js

--- react-emotion-diary/src/components/DiaryItem.js
+++ react-emotion-diary/src/components/DiaryItem.js
@@ -1,5 +1,13 @@
-const DiaryItem = () => {
-  return <div className="DiaryItem"></div>;
+const DiaryItem = ({ id, emotion, content, date } ) => {
+  return <div className="DiaryItem">
+    <div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`]}>
+      <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt="emotion_image" />
+    </div>
+    <div>
+        
+    </div>
+    <div></div>
+  </div>;
 }
 
 export default DiaryItem;
