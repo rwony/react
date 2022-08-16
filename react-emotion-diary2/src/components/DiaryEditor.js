ebaaf4f066e9a08cdd6796a976c77776c3691c31
rwony <88661435+rwony@users.noreply.github.com>
2022-08-16 21:21:25 +0900

    Add files via upload

--- react-emotion-diary2/src/components/DiaryEditor.js
+++ react-emotion-diary2/src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { DiaryDispatchContext } from "../App";
@@ -7,39 +7,37 @@ import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 
+import { getStringDate } from "../util/date.js";
+
 const emotionList = [
   {
     emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `assets/emotion1.png`,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
     emotion_descript: "완전 좋음",
   },
   {
     emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `assets/emotion2.png`,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
     emotion_descript: "좋음",
   },
   {
     emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `assets/emotion3.png`,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
     emotion_descript: "보통",
   },
   {
     emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `assets/emotion4.png`,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
     emotion_descript: "나쁨",
   },
   {
     emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `assets/emotion5.png`,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
     emotion_descript: "끔찍함",
   },
 ];
 
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
-
-const DiaryEditor = () => {
+const DiaryEditor = ({ isEdit, originData }) => {
   const navigate = useNavigate();
   const contentRef = useRef();
 
@@ -47,7 +45,7 @@ const DiaryEditor = () => {
   const [emotion, setEmotion] = useState(3);
   const [content, setContent] = useState("");
 
-  const { onCreate } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
 
   // 작성 버튼 클릭 시 동작하는 함수
   const handleSubmit = () => {
@@ -56,7 +54,18 @@ const DiaryEditor = () => {
       return;
     }
 
-    onCreate(date, content, emotion);
+    if (
+      window.confirm(
+        isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?"
+      )
+    ) {
+      if (!isEdit) {
+        onCreate(date, content, emotion);
+      } else {
+        onEdit(originData.id, date, content, emotion);
+      }
+    }
+
     navigate("/", { replace: true });
   };
 
@@ -64,10 +73,18 @@ const DiaryEditor = () => {
     setEmotion(emotion);
   };
 
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
+
   return (
     <div className="DiaryEditor">
       <MyHeader
-        headText={"새 일기 쓰기"}
+        headText={isEdit ? "일기 수정하기" : "새 일기 쓰기"}
         leftChild={<MyButton text={"<"} onClick={() => navigate(-1)} />}
       />
       <div>
--- react-emotion-diary2/src/pages/Edit.js
+++ react-emotion-diary2/src/pages/Edit.js
@@ -1,7 +1,35 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const diaryList = useContext(DiaryStateContext);
+  const [originData, setOriginData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate("/", { replace: true });
+      }
+    } else {
+      navigate("/", { replace: true });
+    }
+  }, [id, diaryList]);
+
   return (
     <div>
-      <h4>Edit 페이지</h4>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
