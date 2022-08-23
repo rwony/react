e1d4484270622085ecc12bb85e57fbfb6746ff22
rwony <88661435+rwony@users.noreply.github.com>
2022-08-24 00:22:05 +0900

    Add files via upload

--- react-emotion-diary2/src/pages/Diary.js
+++ react-emotion-diary2/src/pages/Diary.js
@@ -1,9 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
+import { DiaryStateContext } from "../App";
+import { getStringDate } from "../util/date.js";
+import { emotionList } from "../util/emotion.js";
+
 const Diary = () => {
-  return (
-    <div>
-      <h4>Diary 상세 페이지</h4>
-    </div>
-  );
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(id) === parseInt(it.id)
+      );
+
+      if (targetDiary) {
+        // 일기가 존재할 때
+        setData(targetDiary);
+      } else {
+        // 일기가 없을 때
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(data.emotion) === parseInt(it.emotion_id)
+    );
+
+    console.log(curEmotionData);
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 일기`}
+          leftChild={<MyButton text={"<"} onClick={() => navigate(-1)} />}
+          rightChild={
+            <MyButton
+              text={"수정"}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+      </div>
+    );
+  }
 };
 
 export default Diary;
